1656ef9e34272a7110d649507c59f14c2d574f75
BohdanT <bohdan1trush@gmail.com>
2020-09-10 23:24:15 +0300

    23:24

--- app/js/model/fetchingData/getEmployersList.js
+++ app/js/model/fetchingData/getEmployersList.js
@@ -1,20 +1,30 @@
 import fetch from './fetchingDataClass'
 import RowEmployer from '../Components/EmployerRow'
-import {list, mount} from '../../../libs/libs'
+import Loader from '../Components/Loader'
+import {list, mount, place} from '../../../libs/libs'
 import initWorkPopup from '../initWorkPopup'
 
-
-
+const loader = place(Loader)
 const row = list("div.rows.worker-rows", RowEmployer);
 mount(document.querySelector('.rows-wrapper'), row);
+mount(document.querySelector('.rows-wrapper'), loader)
+
+const sleep = (ms) => {
+	return new Promise(res => {
+		setTimeout(function(){
+			res('ok')
+		}, ms)
+	})
+}
 
 const getEmployersList = async () => { 
+	loader.update(true)
 	try {
+			// const delay = await sleep(8000)
 			const data = await fetch.getResourse('/employers/get_all')
 			const employers = data.data
-			
 			row.update(employers);
-
+			loader.update(false)
 		// setTimeout(()=> {
 		// 	row.update([...employers.slice(0, 2), ...employers.slice(2 + 1)]);
 		// }, 5000)
